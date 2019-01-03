89b94920a9d2fcdc09355011ba5cb443d0666627
aleksandrpopov <popov_al@tochka.com>
2019-01-03 12:39:05 +0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='Popov Aleksandr',
     version='0.1dev',
-    packages=['requests', ],
+    install_requires=['requests', ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
