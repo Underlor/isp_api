07330f79027a022ce80df646e0ef33e997ff43fb
aleksandrpopov <popov_al@tochka.com>
2019-04-19 12:12:22 +0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='Popov Aleksandr',
-    version='0.2dev',
+    name='ISP SDK',
+    version='0.3',
+    author="Popov Aleksander",
+    author_email="admin@alexue4.ru",
+    url="https://github.com/Underlor/isp_api",
+    description="ISP Panel Api SDK",
     install_requires=['requests', ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
