39f207a1af6f3942126f627f00303c2551d2f0a7
aleksandrpopov <popov_al@tochka.com>
2019-04-19 11:55:03 +0500

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Popov Aleksandr',
-    version='0.1dev',
+    version='0.2dev',
     install_requires=['requests', ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
