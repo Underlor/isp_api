a7abdcf4098497910780681a3d6d7feb70c15789
aleksandrpopov <popov_al@tochka.com>
2019-04-22 08:57:53 +0500

    Add string stub documentation for methods

--- isp_sdk/methods.py
+++ isp_sdk/methods.py
@@ -17,45 +17,62 @@ class Methods:
         response = self.session.send_request(func, params)
         return response
 
-    def del_domain(self, elid: str):
+    def del_domain(self, domain_name: str):
         """
-        :param elid: Domain name
+        :param domain_name: Domain name
         """
         func = 'webdomain.delete'
         params = {
-            'elid': elid,
+            'elid': domain_name,
         }
 
         response = self.session.send_request(func, params)
         return response
 
-    def edit_user(self, name, password, **input_params):
+    def edit_user(self, name: str, **input_params):
+        """
+        Управление учетными записями пользователей
+        Данная функция одновременно используется для просмотра параметров объекта,
+        изменения объекта и создания нового объекта.
+
+        :param name: Логин
+        :param input_params: дополнительные параметры
+        """
         func = 'user.edit'
         params = {
             'sok': 'ok',
             'name': name,
-            'passwod': password,
             **input_params,
         }
 
         response = self.session.send_request(func, params)
         return response
 
-    def edit_email(self, name, domainname, password, **input_params):
+    def edit_email(self, name: str):
+        """
+        Управление почтовыми ящиками
+
+        Документация метода:
+        https://doc.ispsystem.ru/index.php/ISPmanager_API#.D0.9F.D0.BE.D1.87.D1.82.D0.BE.D0.B2.D1.8B.D0.B5_.D1.8F.D1.89.D0.B8.D0.BA.D0.B8
+
+        :param: name: один или несколько уникальных идентификаторов объекта,
+        разделенных запятой и следующим за ней пробелом ", ".
+        Уникальный идентификатор - это элемент name из функции email.
+        """
+
         func = 'email.edit'
         params = {
             'sok': 'ok',
             'name': name,
-            'domainname': domainname,
-            'passwd': password,
-            'confirm': password,
-            **input_params,
         }
 
         response = self.session.send_request(func, params)
         return response
 
     def email_delete(self, name):
+        """
+        Удаление почтового ящика
+        """
         func = 'email.delete'
         params = {
             'elid': name,
